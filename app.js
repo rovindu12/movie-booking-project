4473f2adcc3469a06ed2360cd4fcf4636efe5760
rovindu12 <150222549+rovindu12@users.noreply.github.com>
2024-01-06 14:51:42 +0000

    update

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from "./routes/user-routes";
-import adminRouter from "./routes/admin-routes";
-import movieRouter from "./routes/movie-routes";
-import bookingsRouter from "./routes/booking-routes";
+import userRouter from './routes/user-routes.js';
+import adminRouter from './routes/admin-routes.js';
+import movieRouter from './routes/movie-routes.js';
+import bookingsRouter from './routes/booking-routes.js';
 import cors from "cors";
 dotenv.config();
 const app = express();
@@ -19,7 +19,7 @@ app.use("/booking", bookingsRouter);
 
 mongoose
   .connect(
-    `mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@cluster0.pq0kw.mongodb.net/?retryWrites=true&w=majority`
+    `mongodb+srv://rovindumanawaduge:${process.env.MONGODB_PASSWORD}@cluster0.pq0kw.mongodb.net/?retryWrites=true&w=majority`
   )
   .then(() =>
     app.listen(5000, () =>
